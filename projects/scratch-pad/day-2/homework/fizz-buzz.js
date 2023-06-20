6362b3c3fa8d46798ee26e0f8f1a75056bcbce33
James Sheppard <jshep23@gmail.com>
2023-06-20 20:30:14 +0000

    fizzbuzz 100

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,9 +12,26 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
-
+    //looks like a set number, so I will try a while loop
+    //init var
+    var num = 1;
+    while (num <= 100) { //make the arguement <= so we can get to 100
+        //to determine if a number is a multiple, we can use the remainder operator and strictly equals 0
+        //since we have three constraints, use an if else chain
+        //we should do the divisible by 3 and 5 first because if we do just divisible by 5 or three, we will not proceed down the chain
+        if (num % 3 === 0 && num % 5 === 0){
+          console.log('FizzBuzz');
+        } else if (num % 3 === 0){ //next is number multiple of 3
+          console.log('Fizz');
+        } else if (num % 5 === 0) { //next is number multiple of 5
+          console.log('Buzz');
+        } else { //otherwise, log number as is
+          console.log(num);
+        }
+        
+        
+        num++ //increment so the code stops at end of loop
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
