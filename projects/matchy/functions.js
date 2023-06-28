3ab03b359e72fdc0bdbbd3afac4314647200dd32
James Sheppard <jshep23@gmail.com>
2023-06-28 18:23:13 +0000

    100

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -54,7 +54,7 @@ function remove(animals, name) {
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function add(animals, animal) {
-    var store = [];
+    
     for (var i = 0; i < animals.length; i++) {
     if (animal.name.length > 0 && animal.species.length > 0 && animal.name !== animals[i].name) {
         animals.push(animal);
