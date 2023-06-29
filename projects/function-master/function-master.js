909c5a9db5c38a5075a395a243e5c76dff9a5b05
James Sheppard <jshep23@gmail.com>
2023-06-29 22:01:26 +0000

    function master 13

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -1,6 +1,9 @@
 //////////////////////////////////////////////////////////////////////
 // Function 1 - Object Values ////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+
+
 //should take in an object and return values in array
 function objectValues(object) {
     //should return array so init a storage array
@@ -122,33 +125,70 @@ function maybeNoises(object) {
 //////////////////////////////////////////////////////////////////////
 // Function 10 - Has Words ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a string of words and a word and return true if <word> is in <string of words>, otherwise return false."
 function hasWord(string, word) {
-
+//we should split(' ') the string of words into an array so we can loop through them and compare
+//init storage variable
+var words = string.split(' ')
+//for loop to loop through words array
+for (var i = 0; i < words.length; i++){
+//if statement to see if words[i] strictly equals word
+    if (words[i] === word) {
+        return true;
+    }
+}
+return false; //return false outside of loop if it does not contail the matching word
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and an object and add the name to the object's friends array then return the object
 function addFriend (name, object) {
-
+    //take object's dot notation of friends' index which is object.friends.length and assign it to name
+    //it will always work because the .length property will alwyas be one index ahead
+    object.friends[object.friends.length] = name;
+    return object;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 12 - Is Friend ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and an object and return true if <name> is a friend of <object> and false otherwise
 function isFriend(name, object) {
-
+    if (Object.keys(object).length === 0){ // I had to add this in later to account for empty arrays
+        return false;
+    }
+    //needs a for in loop to loop through the object friends array
+    for (var i = 0; i < object.friends.length; i++){
+        if (object.friends[i] === name) {
+            return true;
+        }
+    }
+    return false;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 13 - Non-Friends /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and a list of people, and return a list of all the names that <name> is not friends with
 function nonFriends(name, array) {
-
+     //init a storage arry to push enemies to
+     var list = [];  
+     var enemy = [];
+       //for loop to loop through array
+       for (var i = 0; i < array.length; i++){
+           //if conditional to check the object.friends and see if they match with name
+         if (array[i].name !== name && array[i].friends.length !== 0) {
+           list.push(array[i]);
+         }
+       }
+       //console.log(list);
+         for (var j = 0; j < list.length; j++) {
+           enemy.push(list[j].name)
+         }
+              
+         return enemy;
 }
 
 //////////////////////////////////////////////////////////////////////
